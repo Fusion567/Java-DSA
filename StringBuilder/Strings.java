a3257da8be645d81183cbec36e498663318e8bbe
Yashrajsinh Parmar <115031337+Fusion567@users.noreply.github.com>
2025-07-30 16:59:51 +0530

    Add files via upload

--- StringBuilder/Strings.java
+++ StringBuilder/Strings.java
@@ -1,1 +1,30 @@
+public class Strings{
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Tony");
+        System.out.println(sb);
 
+        //char at index 0
+        System.out.println(sb.charAt(0));
+        
+        //set char at index 0
+        sb.setCharAt(0,'P');
+        System.out.println(sb);
+
+        //insert element into string
+        sb.insert(3,'n');
+        System.out.println(sb);
+
+        //delete a character from a string
+        sb.delete(2,4);
+        System.out.println(sb);
+
+        //append: add element at the end
+        StringBuilder SB = new StringBuilder("h");
+        SB.append("e");
+        SB.append("l");
+        SB.append("l");
+        SB.append("o");
+        System.out.println(SB);
+        System.out.println(SB.length());
+    }
+}
